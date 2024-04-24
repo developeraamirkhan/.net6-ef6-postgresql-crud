5cf7bb51287c476e76277763565c8cabf34a2e8f
Aamir Khan <developer.aamir.khan@gmail.com>
2024-04-24 22:59:18 +0500

    Add project files.

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestCoreProject.Models;
+using System;
+using Microsoft.Extensions.Options;
+using System.Configuration;
+
+namespace TestCoreProject.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            //AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+        }
+        public DbSet<Student> Students { get; set; } = null!;
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace TestCoreProject.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        public DateTime DateOfBirth { get; set; }
+
+        [StringLength(30, ErrorMessage = "Invalid Location")]
+        public string LocationPin { get; set; } = null!;
+
+        [Required]
+        public string HistoryJson { get; set; } = null!;
+
+        [Required]
+        public string[] Interests { get; set; } = null!;
+    }
+}
